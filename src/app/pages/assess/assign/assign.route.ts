0b142ab05f6b92b568174ac26b69aa2f15b75568
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-08 15:00:08 +0530

    add assign edit component as copy of company-edit component for both add and edit

--- src/app/pages/assess/assign/assign.route.ts
+++ src/app/pages/assess/assign/assign.route.ts
@@ -5,4 +5,14 @@ export const assignRoute: Route[] = [
     path: '',
     loadComponent: () => import('./assign.component').then(c => c.AssignComponent),
   },
+  {
+    path: ':id/edit',
+    loadComponent: () =>
+      import('./assign-edit/assign-edit.component').then(c => c.AssignEditComponent),
+  },
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('./assign-edit/assign-edit.component').then(c => c.AssignEditComponent),
+  },
 ]
