efc33358a4cde96c6ddffe093137273b69c95f14
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-18 06:04:49 +0530

    add data parameter to assign routes

--- src/app/pages/assess/assign/assign.route.ts
+++ src/app/pages/assess/assign/assign.route.ts
@@ -1,18 +1,43 @@
 import { Route } from '@angular/router';
+import { Authority } from 'src/app/constants/authority.constants';
 
 export const assignRoute: Route[] = [
   {
     path: '',
     loadComponent: () => import('./assign.component').then(c => c.AssignComponent),
+    data: {
+      authorities: [
+        Authority.ACCOUNT_ADMIN,
+        Authority.NEA_ADMIN,
+        Authority.ADMIN,
+        Authority.SUPER_ADMIN,
+      ],
+    },
   },
   {
     path: ':id/edit',
     loadComponent: () =>
       import('./assign-edit/assign-edit.component').then(c => c.AssignEditComponent),
+    data: {
+      authorities: [
+        Authority.ACCOUNT_ADMIN,
+        Authority.NEA_ADMIN,
+        Authority.ADMIN,
+        Authority.SUPER_ADMIN,
+      ],
+    },
   },
   {
     path: 'add',
     loadComponent: () =>
       import('./assign-edit/assign-edit.component').then(c => c.AssignEditComponent),
+    data: {
+      authorities: [
+        Authority.ACCOUNT_ADMIN,
+        Authority.NEA_ADMIN,
+        Authority.ADMIN,
+        Authority.SUPER_ADMIN,
+      ],
+    },
   },
 ]
