0fc0c34e94be79e4295a334b545b45ea8942600c
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-18 07:30:22 +0530

    separate out assign assessment component

--- src/app/pages/assess/assign/assign.component.ts
+++ src/app/pages/assess/assign/assign.component.ts
@@ -2,27 +2,25 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { NgxSpinnerService } from 'ngx-spinner';
 import { TabViewModule } from 'primeng/tabview';
-import { Observable, finalize } from 'rxjs';
-import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
+import { Observable } from 'rxjs';
 import { TableComponent } from 'src/app/components/table/table.component';
 import { ACTION_ICON, Action, ColDef } from 'src/app/components/table/table.model';
 import { API_URL } from 'src/app/constants/api-url.constants';
 import { Assessment } from '../assess.model';
 import { AssignService } from './assign.service';
+import { CompanyAssessmentComponent } from './company-assessment/company-assessment.component';
 
 
 @Component({
   selector: 'nl-assign',
   standalone: true,
-  imports: [CommonModule, SpinnerComponent, TableComponent, RouterLink, TabViewModule],
+  imports: [CommonModule , TableComponent, RouterLink, TabViewModule, CompanyAssessmentComponent],
   templateUrl: './assign.component.html',
   styleUrls: ['./assign.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AssignComponent {
-  private spinner = inject(NgxSpinnerService);
   private http = inject(HttpClient);
   private router = inject(Router);
   private assignService = inject(AssignService);
@@ -57,10 +55,7 @@ export class AssignComponent {
 
 
   constructor() {
-    this.spinner.show(this.spinnerName);
-    this.assessments$ = this.assignService.getAssessments().pipe(
-      finalize(() => this.spinner.hide(this.spinnerName))
-    );
+    this.assessments$ = this.assignService.getAssessments();
     this.assessmentGroups$ = this.http.get<any[]>(API_URL.assignGroup);
 
     this.actionsList = [
