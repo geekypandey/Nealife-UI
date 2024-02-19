6558a017b5bb6d45689652f653d0a6b0ea3feb3a
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-19 07:09:59 +0530

    add tab view in association component

--- src/app/pages/assess/association/association.component.ts
+++ src/app/pages/assess/association/association.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+import { AspectItemComponent } from './aspect-item/aspect-item.component';
+import { AssessmentCompetencyComponent } from './assessment-competency/assessment-competency.component';
+import { CompetencyAspectComponent } from './competency-aspect/competency-aspect.component';
+import { GroupAssessmentsComponent } from './group-assessments/group-assessments.component';
 
 @Component({
   selector: 'nl-association',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TabViewModule, AspectItemComponent, AssessmentCompetencyComponent, CompetencyAspectComponent, GroupAssessmentsComponent],
   templateUrl: './association.component.html',
   styleUrls: ['./association.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
