ed099dba71528150ed71b627da1cac05ea3ced5a
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-19 06:58:22 +0530

    add custom assessment page

--- src/app/pages/assess/configure/custom-assessment/custom-assessment.component.ts
+++ src/app/pages/assess/configure/custom-assessment/custom-assessment.component.ts
@@ -1,14 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TableComponent } from 'src/app/components/table/table.component';
+import { ACTION_ICON, Action, ColDef } from 'src/app/components/table/table.model';
+import { API_URL } from 'src/app/constants/api-url.constants';
+import { Assessment } from '../../assess.model';
+
 
 @Component({
   selector: 'nl-custom-assessment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableComponent, RouterLink],
   templateUrl: './custom-assessment.component.html',
   styleUrls: ['./custom-assessment.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CustomAssessmentComponent {
+  assessments$: Observable<Array<Assessment>>;
+  activatedRoute = inject(ActivatedRoute);
+
+  private http = inject(HttpClient);
+  private router = inject(Router)
+
+  cols: ColDef[] = [
+    { header: 'Id', field: 'id' },
+    { header: 'Name', field: 'displayName'},
+    { header: 'Status', field: 'status'},
+  ]
+
+  actionsList: Action[] = [
+      {
+        icon: ACTION_ICON.EDIT,
+        field: 'id',
+        onClick: (value: string) => {
+          this.router.navigate([value + '/edit'], {
+            relativeTo: this.activatedRoute,
+          });
+        },
+      },
+      {
+        icon: ACTION_ICON.DELETE,
+        field: 'id',
+        onClick: (value: string) => {
+          this.router.navigate([value + '/edit'], {
+            relativeTo: this.activatedRoute,
+          });
+        },
+      },
+  ]
+
+  constructor() {
+    this.assessments$ = this.http.get<any>(`${API_URL.assessments}?page=0&size=5000&sort=id,desc`);
+  }
 
 }
+
