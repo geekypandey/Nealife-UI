ac2d76f52d65e90420f7a043ae0b07bca4e2ecaf
kpathe <pathe.kiran@gmail.com>
2024-02-24 18:43:34 +0530

    company detail

--- src/app/pages/assess/company/company-detail/company-detail.component.ts
+++ src/app/pages/assess/company/company-detail/company-detail.component.ts
@@ -4,9 +4,10 @@ import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable, finalize } from 'rxjs';
 import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
+import { USER_ROLE } from 'src/app/constants/user-role.constants';
 import { Company } from '../../assess.model';
+import { ProfileService } from '../../services/profile.service';
 import { CompanyService } from '../company.service';
-
 
 @Component({
   selector: 'nl-company-detail',
@@ -14,21 +15,22 @@ import { CompanyService } from '../company.service';
   imports: [CommonModule, SpinnerComponent],
   templateUrl: './company-detail.component.html',
   styleUrls: ['./company-detail.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CompanyDetailComponent {
-
   spinnerName = 'company-detail';
   company$: Observable<Company>;
 
   private activatedRoute = inject(ActivatedRoute);
   private companyService = inject(CompanyService);
   private spinner = inject(NgxSpinnerService);
+  profileService = inject(ProfileService);
 
   constructor() {
     const companyId = this.activatedRoute.snapshot.params['id'];
     this.spinner.show(this.spinnerName);
-    this.company$ = this.companyService.getCompany(companyId)
+    this.company$ = this.companyService
+      .getCompany(companyId)
       .pipe(finalize(() => this.spinner.hide(this.spinnerName)));
   }
 
@@ -36,4 +38,7 @@ export class CompanyDetailComponent {
     window.history.back();
   }
 
+  get userRole() {
+    return USER_ROLE;
+  }
 }
