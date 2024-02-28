6ac77db8498e3bb9f27d07168c056d3cae519fc6
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-28 09:31:37 +0530

    add functionality to delete single/multiple assessment competency

--- src/app/pages/assess/association/assessment-competency/assessment-competency.component.ts
+++ src/app/pages/assess/association/assessment-competency/assessment-competency.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Observable } from 'rxjs';
 import { TableComponent } from 'src/app/components/table/table.component';
 import { ACTION_ICON, Action, ColDef } from 'src/app/components/table/table.model';
@@ -22,6 +23,8 @@ export class AssessmentCompetencyComponent {
 
   private http = inject(HttpClient);
   private router = inject(Router);
+  private toastService = inject(MessageService);
+  private confirmationService = inject(ConfirmationService);
 
   cols: ColDef[] = [
     { header: 'Id', field: 'id'},
@@ -43,9 +46,24 @@ export class AssessmentCompetencyComponent {
         icon: ACTION_ICON.DELETE,
         field: 'id',
         onClick: (value: string) => {
-          this.router.navigate([value + '/edit'], {
-            relativeTo: this.activatedRoute,
-          });
+          this.confirmationService.confirm({
+            message: `Are you sure that you want to delete Assessment Competency ${value}?`,
+            header: 'Confirm Delete Operation',
+            icon: 'pi pi-info-circle',
+            accept: () => {
+              this.http.delete(`${API_URL.assessmentCompetencies}/${value}`).subscribe({
+                next: () => {
+                  this.toastService.add({
+                    severity: 'success',
+                    summary: `Assessment Competency ${value} successfully deleted`
+                })
+                }
+              })
+              // TODO: complete functionality to refresh the page
+              this.assessmentCompetencies$ = this.http.get<any[]>(API_URL.assessmentCompetencies);
+            },
+            reject: () => {}
+          })
         },
       },
   ]
@@ -56,5 +74,28 @@ export class AssessmentCompetencyComponent {
 
   updateSelection(items: any) {
     this.selectedItems = items;
+  }
+
+  deleteSelectedItems() {
+    this.confirmationService.confirm({
+      message: `Are you sure that you want to delete selected ${this.selectedItems.length} Assessment Competency?`,
+      header: 'Confirm Delete Operation',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        for (const item of this.selectedItems) {
+          this.http.delete(`${API_URL.assessmentCompetencies}/${item}`).subscribe({
+            next: () => { },
+            error: () => {}
+          })
+        }
+        this.toastService.add({
+          severity: 'success',
+          summary: `${this.selectedItems.length} Assessment Competency successfully deleted`
+        })
+        // TODO: complete functionality to refresh the page
+        this.assessmentCompetencies$ = this.http.get<any[]>(API_URL.assessmentCompetencies);
+      },
+      reject: () => {}
+    })
   }
 }
