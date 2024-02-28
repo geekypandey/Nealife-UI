1bbde64e43102e4da8c2ef7f2a331e7fc7bae8a6
Anurag Pandey <anuragcarbon1998@gmail.com>
2024-02-28 10:11:26 +0530

    add delete option for lookup

--- src/app/pages/assess/lookup/lookup.component.ts
+++ src/app/pages/assess/lookup/lookup.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Observable, finalize } from 'rxjs';
 import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
 import { TableComponent } from 'src/app/components/table/table.component';
@@ -25,6 +27,10 @@ export class LookupComponent {
   private spinner = inject(NgxSpinnerService);
   private crudService: CRUDService = inject(CRUDService);
   private router = inject(Router);
+  selectedItems: Array<string> = [];
+  private http = inject(HttpClient);
+  private toastService = inject(MessageService);
+  private confirmationService = inject(ConfirmationService);
   activatedRoute = inject(ActivatedRoute);
 
   cols: ColDef[] = [
@@ -50,6 +56,46 @@ export class LookupComponent {
           });
         },
       },
+      {
+        icon: ACTION_ICON.DELETE,
+        field: 'id',
+        onClick: (value: string) => {
+          this.deleteItems([value]);
+        },
+      },
     ];
+  }
+
+  updateSelection(items: any) {
+    this.selectedItems = items;
+  }
+
+  deleteItems(ids: Array<string>) {
+    this.confirmationService.confirm({
+      message: ids.length == 1 ? `Are you sure that you want to delete LookUp ${ids[0]}?`: `Are you sure that you want to delete selected ${ids.length} LookUp?`,
+      header: 'Confirm Delete Operation',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        for (const id of ids) {
+          this.http.delete(`${API_URL.lookup}/${id}`).subscribe({
+            next: () => { },
+            error: () => {}
+          })
+        }
+        this.toastService.add({
+          severity: 'success',
+          summary: ids.length == 1 ? `LookUp ${ids[0]} successfully deleted` : `${ids.length} LookUp successfully deleted`
+        })
+        // TODO: complete functionality to refresh the page
+        this.lookup$ = this.crudService
+          .query<Lookup[]>(API_URL.lookup)
+          .pipe(finalize(() => this.spinner.hide(this.spinnerName)));
+      },
+      reject: () => {}
+    })
+  }
+
+  deleteSelectedItems() {
+    this.deleteItems(this.selectedItems);
   }
 }
